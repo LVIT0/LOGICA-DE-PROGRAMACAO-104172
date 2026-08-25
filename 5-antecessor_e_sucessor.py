f6cdeac2d979b5e94c40b43ece50d3a42f11228e
LVIT0 <leandro.vitor33pereira@gmail.com>
2026-08-25 14:27:08 -0300

    Add files via upload

---
+++ 5-antecessor_e_sucessor.py
@@ -0,0 +1,14 @@
+import os
+os.system("cls")
+
+#SOLICITANDO DADOS.
+
+numero = int(input('digite um numero:'))
+
+#CALCULANDO
+
+antecessor = numero - 1
+sucessor = numero + 1
+
+print('Antecessor:', antecessor)
+print('Sucessor', sucessor)
