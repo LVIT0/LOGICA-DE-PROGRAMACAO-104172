a0b053be848c1aa036ea382c94e182825a22d830
LVIT0 <leandro.vitor33pereira@gmail.com>
2026-09-01 13:52:17 -0300

    Add files via upload

---
+++ peso.py
@@ -0,0 +1,23 @@
+import os
+os.system
+
+altura = float(input('digite sua altura: '))
+peso = float(input('digite seu peso: '))
+
+imc = peso / (altura * 2)
+print(f'Seu IMC é de:, {imc:.2f}')
+
+if imc <= 18.5:
+    print('Abaixo do peso. ')
+elif imc <= 24.9:
+    print('Peso ideal.(Parabéns!)')
+elif imc <= 29.9:
+    print('Levemente acima do peso.')
+elif imc <= 34.9:
+    print('Obesidade grau 1.')
+
+elif imc <= 39.9:
+    print('Obesidade grau 2 (severa).')
+
+elif imc >= 40:
+    print('Obesidade grau 3 (mórbida).')
